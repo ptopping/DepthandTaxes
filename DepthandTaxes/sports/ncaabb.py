c083bf58221242bc8256a28c5c455b6c575b3d9d
ptopping <ptopping@gmail.com>
2018-12-04 17:47:06 -0700

    Update ncaabb.py

--- DepthandTaxes/sports/ncaabb.py
+++ DepthandTaxes/sports/ncaabb.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import seaborn as sns
 
 x = pd.read_html('http://www.sports-reference.com/cbb/play-index/tourney.cgi?request=1&match=single&year_min=1983&year_max=1983&seed_cmp=eq&opp_seed_cmp=eq&game_result=W&pts_diff_cmp=eq&order_by_single=date_game&order_by_combined=g')
 x
@@ -28,4 +29,10 @@ secondround = results_df[((results_df['Round']=='Second Round') & ((results_df['
 ((results_df['Round']=='Third Round') & ((results_df['Year'] >= 2011) & (results_df['Year'] <= 2015)))]
 
 games = secondround[['Year','WinnerSeed','LoserSeed']].melt(id_vars='Year')
+games.loc[games['variable'] == 'WinnerSeed':,'WIN2'] = 1
+games['WIN2'].fillna(0,inplace=True)
+
+g = sns.catplot(x='WinnerSeed',y='WIN2', data=games, hue='LoserSeed', order = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'], kind='bar')
+g.set_ylabels('2nd Round Win Percentage')
+
 
